ac147402c3c399422d7021a7b4f3b190f62d54eb
Nair Paez <nairpaez@outlook.com>
2026-03-13 19:24:52 -0300

    EJ-10- cambio de logica y ejercicio finalizado

--- src/10-rowAndColumns.js
+++ src/10-rowAndColumns.js
@@ -6,20 +6,19 @@ const pantalla = document.getElementById("pantalla");
 // let salida = true
 
 if (!(isNaN(filas) && filas !== 0) || !(!isNaN(columnas) && columnas !== 0)) {
-  document.writeln('<table class="table table-striped text-center"><tbody">') 
-  let contador= 35
-      for(let i=0; i<filas; i++){
-          document.writeln("<tr>")
-          
-          for(let j=0; j<columnas; j++){
-              document.writeln(`<td>${contador}</td>`)
-              contador--
-          }
-           document.writeln("</tr>")
-      }
-    
-  document.writeln("</tbody></table>")
-
-  }else{
-      alert('Algún dato es inválido: Recuerde que los valors deben ser numéricos y distintos de 0')
+  document.writeln('<table class="table table-striped text-center"><tbody">');
+  let contador = 35;
+  for (let i = 0; i < filas; i++) {
+    document.writeln("<tr>");
+for (let j = 0; j < columnas; j++) {
+      document.writeln(`<td>${contador}</td>`);
+      contador--;
+    }
+    document.writeln("</tr>");
+  }
+document.writeln("</tbody></table>");
+} else {
+  alert(
+    "Algún dato es inválido: Recuerde que los valors deben ser numéricos y distintos de 0",
+  );
 }
