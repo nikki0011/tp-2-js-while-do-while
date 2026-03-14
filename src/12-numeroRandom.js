4fb5fe265565ff1c2dadd20aabc320fcb45e07d9
Nair Paez <nairpaez@outlook.com>
2026-03-13 21:09:41 -0300

    EJ-12- numero random de 1 a 99 con math.ceil y math random

--- src/12-numeroRandom.js
+++ src/12-numeroRandom.js
@@ -1,7 +1,7 @@
 // * 12- Realiza un script que genere un número aleatorio entre 1 y 99
 
-const pantalla = document.getElementById('pantalla')
+const pantalla = document.getElementById("pantalla");
 
-let numRandom= Math.ceil(Math.random()*99)
+let numRandom = Math.ceil(Math.random() * 99);
 
-pantalla.innerHTML = `Número Random de 1 a 99: <span class='text-info'>${numRandom}</span>`
+pantalla.innerHTML = `Número Random de 1 a 99: <span class='text-info'>${numRandom}</span>`;
