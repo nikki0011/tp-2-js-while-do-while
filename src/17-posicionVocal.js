02ed76611204501254a207be5498f9cda9649870
Nair Paez <nairpaez@outlook.com>
2026-03-14 18:52:40 -0300

    EJ-17- posicion de la primera vocal de un cadena de texto ingresada por el usuario

--- src/17-posicionVocal.js
+++ src/17-posicionVocal.js
@@ -1,17 +1,20 @@
-// *15- Realiza un script que cuente el número de vocales que tiene un texto.
+// * 17- Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.
 
 const pantalla = document.getElementById("pantalla");
 const palabraVocales = prompt("Ingrese texto").toLowerCase();
 const vocales = "aeiouáéíóú";
-let cantVocales = 0;
+let primeraVocal = "";
 
 for (let i = 0; i < palabraVocales.length; i++) {
   for (let j = 0; j < vocales.length; j++) {
     if (palabraVocales[i] === vocales[j]) {
-      cantVocales++;
+      primeraVocal += palabraVocales[i];
     }
   }
 }
+
+primeraVocal = primeraVocal.charAt(0);
+let posicionPrimeraVocal = palabraVocales.indexOf(primeraVocal);
 pantalla.innerHTML =
   "<span class='text-secundary'>Texto ingresado: " +
   "<span class='text-warning'>" +
@@ -19,8 +22,10 @@ pantalla.innerHTML =
   "</span>" +
   "</span>" +
   "<br/>" +
-  "<span class='text-secundary'>Cantidad de vocales: " +
+  "<span class='text-secundary'>La vocal '" +
+  primeraVocal +
+  "' esta en la posición: " +
   "<span class='text-warning'>" +
-  cantVocales +
+  posicionPrimeraVocal +
   "</span>" +
   "</span>";
